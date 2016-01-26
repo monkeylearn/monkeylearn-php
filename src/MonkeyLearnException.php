61532d4ede9232b1acd4e7817aca569abb7e8dfa
rstecanella <rstecanella@tryolabs.com>
2016-01-26 16:37:25 -0300

    bugfix, Exception class was not imported properly

--- src/MonkeyLearnException.php
+++ src/MonkeyLearnException.php
@@ -1,8 +1,8 @@
 <?php
 namespace MonkeyLearn;
 
-class MonkeyLearnException extends Exception {
-    public function __construct($message = null, $code = 0, Exception $previous = null){
+class MonkeyLearnException extends \Exception {
+    public function __construct($message = null, $code = 0, \Exception $previous = null){
         parent::__construct('Error: '.$message, $code, $previous);
     }
 }
